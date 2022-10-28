7c4affa94b9fcf38e568b2aa83dbf0618093a289
leht377 <migithub2019@gmail.com>
2022-10-28 10:02:03 -0500

    types_cheking biseccion

--- metnum/mRaices/decoradores/types_cheking.py
+++ metnum/mRaices/decoradores/types_cheking.py
@@ -1,21 +1,24 @@
 def args_type_checking(fn):
     def wrapper(*args):
-        # if not isinstance(args[0],)
 
         argumentos = {
+            "f(x)": args[0],
             "intervaloA": args[1],
             "intervaloB": args[2],
             "tolerancia": args[3],
         }
 
+        if not callable(argumentos["f(x)"]):
+            raise TypeError("El objeto no es invocable")
+
         if not isinstance(argumentos["intervaloA"], (int, float)):
-            raise TypeError("Todos los argumentos deben ser enteros o floats")
+            raise TypeError("El intervaloA deben ser enteros o floats")
 
         if not isinstance(argumentos["intervaloB"], (int, float)):
-            raise TypeError("Todos los argumentos deben ser enteros o floats")
+            raise TypeError("El intervaloB deben ser enteros o floats")
 
         if not isinstance(argumentos["tolerancia"], (int, float)):
-            raise TypeError("Todos los argumentos deben ser enteros o floats")
+            raise TypeError("El intervaloC deben ser enteros o floats")
 
         return fn(*args)
 
