848c18de6be93b6b8cc617e43f927492b2309997
leht377 <migithub2019@gmail.com>
2023-03-05 19:02:23 -0500

    conf general

--- script.py
+++ script.py
@@ -1,43 +1,29 @@
 from metnum import biseccion, reglaFalsa, newtonRapson, secante
 import matplotlib.pyplot as plt
-
+import math
 
 # print(
 #     "BISECCION: ",
 #     biseccion(lambda x: -1 * x**3 + 3 * x - 4, -3, -1.5, 10**-6, True),
 # )
-# print(
-#     "REGLA FALSA: ",
-#     reglaFalsa(lambda x: -1 * x**3 + 3 * x - 4, -3, -1.5, 10**-6, False),
-# )
-# print(
-#     "NEWTON RAPSON: ",
-#     newtonRapson(
-#         lambda x: -1 * x**3 + 3 * x - 4,
-#         lambda x: -3 * x**2 + 3,
-#         -1.5,
-#         10**-6,
-#         20,
-#         False,
-#     ),
-# )
 
-print(
-    "SECANTE: ",
-    secante(lambda x: -1 * x**3 + 3 * x - 4, -3, -4, 10**-6, 50, True),
-)
 # print(
 #     "REGLA FALSA: ",
-#     reglaFalsa(lambda x: x**3 - 1 * x - 1, 1, 1.8, 10**-6, False),
+#     reglaFalsa(lambda x: -1 * x**3 + 3 * x - 4, -3, -1.5, 10**-6, True),
 # )
 # print(
 #     "NEWTON RAPSON: ",
 #     newtonRapson(
-#         lambda x: x**7 - 4 * x**4 - x,
-#         lambda x: 7 * x**6 - 16 * x**3,
-#         4,
+#         lambda x: x**3 - 2 * x + 2,
+#         lambda x: 3 * x**2 - 2,
+#         0,
 #         10**-6,
-#         20,
-#         False,
+#         40,
+#         True,
 #     ),
+# )
+# secante(lambda x: x**3 - x**2, 1, 0, 10**-6, 50, True), PROBLEMA DIVISION CON CERO
+# print(
+#     "SECANTE: ",
+#     secante(lambda x: x**3 - x**2, 4, 8, 10**-6, 50, True),
 # )
