a1c4cb98cdf757af421a67e17d71f958ba9f1dd9
leht377 <migithub2019@gmail.com>
2023-03-06 11:11:50 -0500

    base para types checking metodos ecuaciones lineales

---
+++ metnum/mEcuacionesLineales/decoradores/__init__.py
@@ -0,0 +1,6 @@
+from .types_checking import (
+    args_types_checking_gauss_seidel,
+    args_types_checking_gauss_jacobi,
+    args_types_checking_gauss_jordan,
+    args_types_checking_gauss_LU,
+)
---
+++ metnum/mEcuacionesLineales/decoradores/types_checking.py
@@ -0,0 +1,14 @@
+def args_types_checking_gauss_seidel(fn):
+    pass
+
+
+def args_types_checking_gauss_jordan(fn):
+    pass
+
+
+def args_types_checking_gauss_jacobi(fn):
+    pass
+
+
+def args_types_checking_gauss_LU(fn):
+    pass
