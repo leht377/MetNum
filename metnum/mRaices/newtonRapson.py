9f7999bea6322c84fa664a5be1c22ded39790dbe
DAEespi <bhgespinel@gmail.com>
2023-03-06 16:07:55 -0500

    Documentacion faltante de ecuaciones lineales

--- metnum/mRaices/newtonRapson.py
+++ metnum/mRaices/newtonRapson.py
@@ -9,25 +9,57 @@ def newtonRapson(
     maximoInteraciones: int or float,
     plot: bool = False,
 ) -> tuple:
-    """ """
+    """
+    Esta función implementa el método de Newton-Raphson para encontrar raíces de una función.
+
+    Parametros
+    -----------
+    f: function
+       Funcion a la que se le va a encontra una raíz.
+    fderivadax: function
+                Derivada de la funcion f.
+    puntoInicial: int or float
+                  Punto de partida para la busqueda de la raíz.
+    toleracia: int or float
+               Tolerancia aceptable del error para la raíz.
+    maximoInteraciones: int or float
+                        Número de interaciones máximas permitidas.
+    (Opcional)  plot: bool
+       Ver graficamente el metodo de newtonRapson.
+
+    Retorna
+    --------
+
+    Una tupla con la raíz encontrada,
+    el valor absoluto del error,
+    el número de iteraciones realizadas.
+
+    Ejemplo
+    --------
+    >>> newtonRapson( lambda x: x**3 - 2 * x + 2,
+                  lambda x: 3 * x**2 - 2, 0, 10**-6, 40, True,
+    ),
+     (0.0, 1.0, 2.0)
+
+    """
     interaciones = 0
     aproxNueva = puntoInicial
 
     f_de_apoxNueva = f(aproxNueva)
     fdx_aproxNueva = fDerivadax(aproxNueva)
 
-    difsuc = f_de_apoxNueva / fdx_aproxNueva
+    dfsuc = f_de_apoxNueva / fdx_aproxNueva
 
     if plot:
         historialRaiz = [aproxNueva]
 
-    while abs(difsuc) >= toleracia and interaciones <= maximoInteraciones:
+    while abs(dfsuc) >= toleracia and interaciones <= maximoInteraciones:
         interaciones += 1
-        aproxNueva = aproxNueva - difsuc
+        aproxNueva = aproxNueva - dfsuc
         f_de_apoxNueva = f(aproxNueva)
         fdx_aproxNueva = fDerivadax(aproxNueva)
 
-        difsuc = f_de_apoxNueva / fdx_aproxNueva
+        dfsuc = f_de_apoxNueva / fdx_aproxNueva
 
         plot and historialRaiz.append(aproxNueva)
 
--- script.py
+++ script.py
@@ -4,15 +4,15 @@ from metnum import biseccion, reglaFalsa, newtonRapson, secante, gaussJordan
 import matplotlib.pyplot as plt
 import math
 
-A = [
-    [2, -1, 4, 1, -1],
-    [-1, 3, -2, -1, 2],
-    [5, 1, 3, -4, 1],
-    [3, -2, -2, -2, -3],
-    [-4, -1, -5, 3, -4],
-]
-b = [[7], [1], [33], [24], [-49]]
-print(gaussJordan(A, b))
+# A = [
+#     [2, -1, 4, 1, -1],
+#     [-1, 3, -2, -1, 2],
+#     [5, 1, 3, -4, 1],
+#     [3, -2, -2, -2, -3],
+#     [-4, -1, -5, 3, -4],
+# ]
+# b = [[7], [1], [33], [24], [-49]]
+# print(gaussJordan(A, b))
 
 # print(
 #     "BISECCION: ",
@@ -34,8 +34,11 @@ print(gaussJordan(A, b))
 #         True,
 #     ),
 # )
-# secante(lambda x: x**3 - x**2, 1, 0, 10**-6, 50, True), PROBLEMA DIVISION CON CERO
-# print(
-#     "SECANTE: ",
-#     secante(lambda x: x**3 - x**2, 4, 8, 10**-6, 50, True),
-# )
+# secante(
+#     lambda x: x**3 - x**2, 1, 0, 10**-6, 50, True
+# ), "PROBLEMA DIVISION CON CERO"
+print(
+    "SECANTE: ",
+    # secante(lambda x: x**3 - x**2, 4, 8, 10**-6, 50, True),
+    secante(lambda x: x**2 - 2, 1, 2, 1e-6, 100, True),
+)
