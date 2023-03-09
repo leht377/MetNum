d313f6708e1f7b3e426e06a9d5d70bf984358990
leht377 <migithub2019@gmail.com>
2023-03-09 10:58:16 -0500

    implementacion metodo LU

--- metnum/mEcuacionesLineales/LU.py
+++ metnum/mEcuacionesLineales/LU.py
@@ -1,5 +1,5 @@
 import numpy as np
-from helpers import sustprogr, sustregr
+from .helpers import sustprogr, sustregr
 
 
 def LU(A, b):
@@ -37,9 +37,3 @@ def LU(A, b):
     x = sustregr(U, y)
 
     return x
-
-
-# A = [[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]]
-# b = [[7.85], [-19.3], [71.4]]
-
-# print(LU(A, b))
