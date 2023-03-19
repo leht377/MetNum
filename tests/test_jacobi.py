5cf64a9e596efe3ef872104a644d25a4af5259ea
=leht377 <=migithub2019@gmail.com>
2023-03-19 13:57:19 -0500

    implementacion parcial test_jacobi

--- tests/test_jacobi.py
+++ tests/test_jacobi.py
@@ -4,14 +4,16 @@ from metnum import jacobi
 
 
 class test_jacobi(unittest.TestCase):
-    def test_retorna_una_tupla(self):
+    def test_encontro_incognita1(self):
         A = [[10, 2, -3], [4, 7, -1], [-2, 1, 4]]
         b = [[1], [-1], [5]]
         x0 = [[0], [0], [0]]
         resultado = jacobi(A, b, x0)
         self.assertIsNone(
             np.testing.assert_allclose(
-                resultado, [[0.64903836], [-0.27884608], [1.64423064]]
+                resultado,
+                [[0.64903836], [-0.27884608], [1.64423064]],
+                atol=10**-5,
             )
         )
 
