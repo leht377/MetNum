19575b3fcc1f77cc13537834e619b74eb9df54fe
=leht377 <=migithub2019@gmail.com>
2023-03-19 13:54:40 -0500

    documentacion jacobi

--- metnum/mEcuacionesLineales/jacobi.py
+++ metnum/mEcuacionesLineales/jacobi.py
@@ -3,32 +3,73 @@ import numpy as np
 
 def jacobi(
     A,
-    b: list[float],
-    x0: list[float],
+    b: list,
+    x0: list,
     tol: int or float = pow(10, -12),
     maxiter: int = 25,
 ):
+    """
+    Esta funcion resuelve sistemas de ecuaciones lineales Ax=b usando el método de jacobi
+
+    Parametros
+    ------------
+    A: list
+        Matriz de coeficiente del sistema de ecuaciones
+    b: list
+        Vector de terminos independientes
+    x0: list
+        Vector de aproximacion inicial 
+    (opcional) tol: int or float
+        Tolerancia maxima con la cual se acepta la soluci+on
+    (opcional) maxiter: int
+        Numero maximo de iteraciones para encontrar una solución
+
+
+    Retorna
+    ------------
+    vectorSolucion: list 
+        Vector con la solucion aproximada al sistema de ecuaciones 
+
+    Ejemplos
+    ----------
+
+    ejemplo #1
+
+    >>> jacobi([[6, 2, 1], [-1, 8, 2], [1, -1, 6]], [[25], [-6], [23]], [[0], [0], [0]], 10^-12, 25)
+    [[-4],[-1],[3]]
+
+    ejemplo #2
+    >>> jacobi([[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]],[[7.85], [-19.3], [71.4]],[[0], [0], [0]],10^-12, 25 )
+    [[3.],[-2.5],[7.]]
+
+    """
     A = np.array(A)
     b = np.array(b)
-    mIncognitas = np.array([x0], dtype=float)
-    (fila, col) = A.shape
-    normb = np.linalg.norm(b)
 
-    temp = [[0], [0], [0]]
+    # diag tiene el siguiente comportamiento si se le pasa solo un vector [..,..,..] este contruira una matriz con estos valores como diagonal
+    # y el resto sera 0
+    # El segundo comportamiento si se le pasa una matriz extraera solo los elementos de la diagonal
+    # Crea una matriz solo con los elementos de la diagonal de la matriz A el resto de las posiciones las llena con 0
+    D = np.diag(np.diag(A))
+    R = A.copy()
+    np.fill_diagonal(R, 0)  # Remplaza la diagonal de la matriz por 0
+
+    mIncognitas = np.array([x0], dtype=float)
+
+    aproxNuevas = [[0], [0], [0]]
     iteracion = 0
 
-    while (
-        np.linalg.norm(np.subtract(np.dot(A, mIncognitas[iteracion]), b)) > tol * normb
-    ) and (iteracion < maxiter):
-        for k in range(fila):
-
-            temp[k][0] = np.divide(
-                b[k]
-                - np.dot(np.delete(A[k, :], k), np.delete(mIncognitas[iteracion], k)),
-                A[k][k],
-            )[0]
-
-        mIncognitas = np.insert(mIncognitas, iteracion + 1, temp, axis=0)
+    while iteracion < maxiter:
+        aproxNuevas = np.dot(np.linalg.inv(
+            D), b - np.dot(R, mIncognitas[iteracion]))
+        mIncognitas = np.insert(
+            mIncognitas, iteracion + 1, aproxNuevas, axis=0)
         iteracion = iteracion + 1
 
-    return mIncognitas[iteracion]
+        E = np.linalg.norm(mIncognitas[iteracion] - mIncognitas[iteracion - 1])
+        if E < tol:
+            break
+
+    vectorSolucion = mIncognitas[iteracion]
+
+    return vectorSolucion
