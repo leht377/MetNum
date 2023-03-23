1cd0acabd6e6fa8cf7dc7c10aade954871ca4a74
=leht377 <=migithub2019@gmail.com>
2023-03-23 11:12:20 -0500

    refactor gaussSeidel

--- metnum/mEcuacionesLineales/gaussSeidel.py
+++ metnum/mEcuacionesLineales/gaussSeidel.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-from .decoradores import args_types_checking_gauss_seidel
-from .helpers import es_diagonal_dominante
+from .decoradores import gaussSeidel_args_types_checking, gaussSeidel_args_transform_np_array
+from .helpers import es_diagonal_dominante, es_matriz_cuadrada
 
 # TODO VERIFICAR SI LA MATRIZ QUE SE LE PASA ES cuadrada
 
 
-@args_types_checking_gauss_seidel
+@gaussSeidel_args_types_checking
+@gaussSeidel_args_transform_np_array
 def gaussSeidel(
     A: list[float],
     b: list[float],
@@ -36,7 +37,7 @@ def gaussSeidel(
     Retorna
     ----------
 
-    x: list
+    x0: list
         Matriz con la solucion al sistema de ecuaciones liniales
 
     Ejemplos
@@ -44,16 +45,15 @@ def gaussSeidel(
 
     ejemplo #1
 
-    >>> gaussSeidel([[6, 2, 1], [-1, 8, 2], [1, -1, 6]], [[25], [-6], [23]], [[0], [0], [0]],10^-12, 25)
+    >>> gaussSeidel([[6, 2, 1], [-1, 8, 2], [1, -1, 6]], [[25], [-6], [23]], [[0], [0], [0]],10**-12, 25)
     [[-4],[-1],[3]]
 
     ejemplo #2
-    >>> gaussSeidel([[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]],[[7.85], [-19.3], [71.4]],[[0], [0], [0]] ,10^-12, 25)
+    >>> gaussSeidel([[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]],[[7.85], [-19.3], [71.4]],[[0], [0], [0]] ,10**-12, 25)
     [[3.],[-2.5],[7.]]
     """
-    A = np.array(A)
-    b = np.array(b)
-    x = np.array(x0, dtype=float)
+    if not (es_matriz_cuadrada(A)):
+        raise ValueError("La matriz A debe de ser cuadrada")
 
     if not (es_diagonal_dominante(A)):
         raise ValueError(
@@ -63,14 +63,14 @@ def gaussSeidel(
     normb = np.linalg.norm(b)
     iter = 0
 
-    while (np.linalg.norm(np.subtract(np.dot(A, x), b)) > tol * normb) and (
+    while (np.linalg.norm(np.subtract(np.dot(A, x0), b)) > tol * normb) and (
         iter < maxiter
     ):
         iter += 1
         for j in range(n):
             resultA = np.concatenate([A[j][0:j], A[j][j + 1: n + 1]])
-            resultX = np.concatenate([x[0:j], x[j + 1: n + 1]])
+            resultX = np.concatenate([x0[0:j], x0[j + 1: n + 1]])
 
-            x[j] = (np.subtract(b[j], np.dot(resultA, resultX))) / A[j][j]
+            x0[j] = (np.subtract(b[j], np.dot(resultA, resultX))) / A[j][j]
 
-    return x
+    return x0
