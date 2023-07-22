7688f4b9b4427dedbf18e57735acd4efea2a7e6b
=leht377 <=migithub2019@gmail.com>
2023-07-22 15:24:02 -0500

    refactor decoradores ecuaciones lineales

--- metnum/mEcuacionesLineales/decoradores/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .types_checking import (
-#     args_types_checking_gauss_jordan,
-#     args_types_checking_gauss_LU,
-# )
-from .gaussSeidelDecorators import gaussSeidel_args_types_checking, gaussSeidel_args_transform_np_array
-from .jacobiDecorators import jacobi_args_types_checking, jacobi_args_transform_np_array
-from .gaussJordaDecorators import gaussJordan_args_transform_np_array, gaussJordan_args_types_checking
--- metnum/mEcuacionesLineales/decoradores/types_checking.py
+++
@@ -1,8 +0,0 @@
-
-
-def args_types_checking_gauss_jordan(fn):
-    pass
-
-
-def args_types_checking_gauss_LU(fn):
-    pass
