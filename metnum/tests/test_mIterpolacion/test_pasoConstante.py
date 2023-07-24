92b726aad56efe9cca4b1e867e27747340d9024e
=leht377 <=migithub2019@gmail.com>
2023-07-24 09:52:11 -0500

    paso costante tests

---
+++ metnum/tests/test_mIterpolacion/test_pasoConstante.py
@@ -0,0 +1,32 @@
+import pytest
+from ...mIterpolacion import pasoConstante
+from ..helpers import mensaje_error
+
+
+@pytest.mark.parametrize(
+    "x,y,xk,expected",
+    [([5, 10, 15, 20, 25, 30, 35, 40], [-25, 0, 75, 200, 375, 600, 875, 1200], 12, 24),  # lambda x: x**2 -10*x
+     ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [1, 8, 27, 64,
+      125, 216, 343, 512, 729, 1000, 1331], 8.5, 614.125),  # lambda x: x**3
+     ([12, 15, 18, 21, 24], [1728, 3375, 5832,
+      9261, 13824], 21, 9261),  # lambda x: x**3
+     ([12, 15, 18, 21, 24], [1728, 3375, 5832, 9261, 13824], 12, 1728)]  # lambda x: x**3
+)
+def test_encontro_yk(x, y, xk, expected):
+    yk, _ = pasoConstante(x, y, xk)
+    assert yk == pytest.approx(expected)
+
+
+@pytest.mark.parametrize(
+    "x,y,xk,expected",
+    [(1, [-25, 0, 75, 200, 375, 600, 875, 1200], 12, mensaje_error("x", "list | numpy.ndarray")),
+     ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 2, 8.5,
+      mensaje_error("y", "list | numpy.ndarray")),
+     ([12, 15, 18, 21, 24], [1728, 3375, 5832,
+      9261, 13824], "21", mensaje_error("xk", "float | int")),
+     ]
+)
+def test_args_types(x, y, xk, expected):
+    with pytest.raises(TypeError) as context:
+        pasoConstante(x, y, xk)
+    assert expected in str(context.value)
